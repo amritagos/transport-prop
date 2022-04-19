45a29b65b16e244b61c5917cde606acdf4d2dd7d
amritagos <amrita16thaug646@gmail.com>
2022-04-19 02:21:47 +0000

    example: Modified python script now calls cli

--- example/run-mw.py
+++ example/run-mw.py
@@ -5,13 +5,12 @@ from ase.atoms import Atom, Atoms # ASE stuff
 import pathlib
 from ase.io import read, write, lammpsrun 
 
-# # Read in the trajectory 
-# wat_traj = read('MWwater-260', format='lammps-dump-text', index=':')
+from transportProp import structures, cli 
 
-wat_traj = read('MWwater-260', format='lammps-dump-text', index=':')
-# print(wat_traj[-1])
+# # Read in the TOML file 
+msd_options = cli.get_msd_data_options('input.toml')
 
-msd_mw = trp.MeanSquaredDisplacement(wat_traj, delta_tau = 10)
-msdList = msd_mw.calculate_msd()
+# # Calculate the MSD and 
+## write out the output files
 
-np.savetxt('msd-xyz.txt', msdList, delimiter=' ') 
+cli.perform_msd_calc(msd_options)
