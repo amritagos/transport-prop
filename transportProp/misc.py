af5baf243048a046bc8e85421f93af66f54c11f2
amritagos <amrita16thaug646@gmail.com>
2022-04-20 00:38:08 +0000

    src: Function for calculating the COM of water molecules. TODO: make more general

--- transportProp/misc.py
+++ transportProp/misc.py
@@ -1,7 +1,9 @@
 # Declare exported functions
-__all__ = [ 'getpath', 'get_slice']
+__all__ = [ 'getpath', 'get_slice', 'com_water_traj']
 
 from pathlib import Path
+from ase.build import molecule
+from ase import Atoms, Atom
 
 def getpath(*a):
     ''' Get the directory in which the current file lives 
@@ -23,3 +25,29 @@ def get_slice(a, dim):
     else:
         # for the option dim='xyz'
         return a 
+
+def com_water_traj(atoms_traj):
+    ''' Given a list of ASE Atoms objects, (read in from a trajectory)
+    and assuming that it is for water and is in the order O H H , 
+    get the center of mass of each molecule, and return a new Atoms object.
+
+    We assume that there are no other atom types in the trajectory, and that the water
+    molecules are perfectly ordered. 
+
+    TODO: Figure out a way to make this more general 
+    '''
+    n_atoms = len(atoms_traj[0]) # Number of atoms in every configuration / frame (assume unchanged in time)
+    wat = molecule('H2O') # Its positions will be set to the current molecule. 
+    new_traj = [] # New trajectory containing the centers of masses of the water molecules 
+
+    for atoms in atoms_traj:
+        pos = atoms.get_positions() # positions of all O H H atoms in the current frame
+        current_frame = Atoms() # center of masses of the current molecule 
+        for i in range(0,n_atoms,3):
+            wat.set_positions(pos[i:i+3, :])
+            com = wat.get_center_of_mass() # Center of mass of the current molecule 
+            current_frame += Atom('O', position = com) # Add com to the current frame 
+        new_traj.append(current_frame) # Append frame to the trajectory 
+
+    # Return the list of the Atoms objects with COM 
+    return new_traj
