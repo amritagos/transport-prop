8f1d3c4ee6b4c2f919607282f30f1a8294f13878
amritagos <amrita16thaug646@gmail.com>
2022-08-17 20:23:36 +0000

    example: TCF example init

---
+++ example/solvation-tcf/run.py
@@ -0,0 +1,16 @@
+import transportProp as trp
+import numpy as np
+import ase
+from ase.atoms import Atom, Atoms # ASE stuff 
+import pathlib
+from ase.io import read, write, lammpsrun 
+
+from transportProp import structures, cli 
+
+# # Read in the TOML file 
+vacf_options = cli.get_vacf_data_options('input.toml')
+
+# # Calculate the VACF and 
+## write out the output files
+
+cli.perform_vacf_calc(vacf_options)
