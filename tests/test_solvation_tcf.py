c0320a8186959a3fff30be5188ea496fa768efb4
amritagos <amrita16thaug646@gmail.com>
2022-08-19 14:17:57 +0000

    tests: Added a test of the solvation TCF class

--- tests/test_solvation_tcf.py
+++ tests/test_solvation_tcf.py
@@ -7,7 +7,7 @@ import pathlib
 from . import aux
 
 @pytest.fixture
-def small_system(): 
+def small_system_energy_diff_array(): 
     ''' Reads in two LAMMPS trajectories of 5 steps, both of which contain two TIP4P/2005 
     water molecules and one 'ground-state' ion, and returns
     a numPy array of the energy differences per atom, with columns corresponding to every 
@@ -28,20 +28,37 @@ def small_system():
     energy_diff = trp.misc.get_energy_difference(traj0, traj1, key_value='c_peratom', start_t0=0)
     return energy_diff
 
-def test_energy_diff_array(small_system):
+@pytest.fixture
+def small_system(small_system_energy_diff_array): 
+    ''' Given the energy differences between two LAMMPS trajectories of 5 steps, both of which contain two TIP4P/2005 
+    water molecules and one 'ground-state' ion, returns a SolvationTimeCorrelation object.
+    The time difference between each frame in the 
+    trajectory is 10 ps (delta_t). The coordinates are in Angstroms, and are *wrapped*. 
+
+    Please note: this is actually a very short trajectory from the perspective of the solvation correlation function
+    This returns an ASE Atoms object, which must be processed further. The solvation correlation function requires
+    the fluctuations in the energy differences as input.
+    '''
+    tcf_small_sys = trp.SolvationTimeCorrelation(small_system_energy_diff_array)
+    return tcf_small_sys
+
+def test_energy_diff_array(small_system_energy_diff_array):
     ''' Tests that the energy difference has been computed correctly, for a system with one ion 
     and two water molecules. 
     Takes in a numPy array 
     '''
-    diff_array = small_system # numPy array of differences 
+    diff_array = small_system_energy_diff_array # numPy array of differences 
     # Should have the shape (7,5)
     assert diff_array.shape == (7,5) 
-    # # Given a time origin (the first frame, index 0 in wat_traj)
-    # # and a lag time of 1 delta_t (difference between time frames)
-    # vel_t0 = wat_traj[0].get_velocities() # Velocities of all atoms in the first frame
-    # vel_t = wat_traj[1].get_velocities() # Velocities of all atoms in the second frame
-    # vacf_t0_t = short_three_mols.v_tau_v_t0(vel_t0, vel_t) # VACF for one t0 and lag time, averaged over particles 
-    # # Check: 
-    # # should be  [ 4.37403330e-09 -1.50198503e-09 -2.53979207e-10]
-    # vacf_tt0_ref = np.array([ 4.37403330e-09, -1.50198503e-09, -2.53979207e-10])
-    # assert vacf_t0_t == pytest.approx(vacf_tt0_ref, 1e-6)
+
+def  test_tcf_all_atoms_t_t0(small_system):
+    ''' Tests the value of the unnormalized TCF , calculated for a particular time origin and lag time, 
+    averaged over all the particles (in this case, 2 water molecules and 1 ion).  
+    Takes in a SolvationTimeCorrelation object instantiated with the energy difference array.
+    '''
+    energy_fluc_array = small_system.energ_fluc # numPy array of energy difference fluctuations (n_atoms, n_frames)
+    # TCF for one t0 and lag time, averaged over particles
+    tcf_t0_t = small_system.e_tau_e_t0(energy_fluc_array[:,0], energy_fluc_array[:,1])  
+    # Check: 
+    tcf_tt0_ref = 11.926569490688 # should be  11.926569490688
+    assert tcf_t0_t == pytest.approx(tcf_tt0_ref, 1e-6)
