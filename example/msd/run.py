81517c9b9d63170daf9aa0ee476cd13b237d7174
amritagos <amrita16thaug646@gmail.com>
2022-12-12 19:22:45 +0000

    examples: Now there is no need to run the code this way and it can be run through the CLI. After installing, just run transportProp

--- example/msd/run.py
+++ example/msd/run.py
@@ -7,10 +7,25 @@ from ase.io import read, write, lammpsrun
 
 from transportProp import structures, cli 
 
-# # Read in the TOML file 
-msd_options = cli.get_msd_data_options('input.toml')
+import typer 
 
-# # Calculate the MSD and 
-## write out the output files
+app = typer.Typer()
 
-cli.perform_msd_calc(msd_options)
+# Subcommands for MSD
+@app.command()
+def msd(tomlfile: str):
+    # Read in the TOML file
+    msd_options = cli.get_msd_data_options(tomlfile)
+    # Calculate the MSD and write out the output files
+    cli.perform_msd_calc(msd_options)
+
+if __name__ == "__main__":
+    app()
+
+# # # Read in the TOML file 
+# msd_options = cli.get_msd_data_options('input.toml')
+
+# # # Calculate the MSD and 
+# ## write out the output files
+
+# cli.perform_msd_calc(msd_options)
