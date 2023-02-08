f880cced77578b37758303fa42bd1a7d586878c3
amritagos <amrita16thaug646@gmail.com>
2023-02-08 17:21:48 +0000

    example: Added example for getting the peak heights and positions with time

---
+++ example/time-dependent-rdf/run.py
@@ -0,0 +1,18 @@
+import transportProp as trp
+from pathlib import Path
+
+from transportProp import structures, util
+
+## For testing with gdb
+# Run the following: 
+# gdb -args python run.py 
+
+input_traj = 'dump/dump-500000.lammpstrj'
+typeO = 1
+typeIon = 3
+cutoff = 15.0 
+binwidth = 0.1 
+
+input_path = Path(input_traj)
+
+peak_pos, peak_heights = util.get_rdf_peakData_time(input_path, typeO, typeIon, binwidth, cutoff)
