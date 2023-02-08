8edd2b7166a469882e5b7b75afcc7757732d2ec2
amritagos <amrita16thaug646@gmail.com>
2023-02-08 17:20:13 +0000

    cpp: Added RDF function for each timestep

--- transportProp/fastcpp/binding.cpp
+++ transportProp/fastcpp/binding.cpp
@@ -5,9 +5,11 @@
 #include <Eigen/Dense>
 
 #include "tcf.hpp"
+#include "rdf.hpp"
 
 PYBIND11_MODULE(fastcpp, m) {
     m.doc() = "Module for correlation function loops"; // module docstring
 
     m.def("time_corr_function", &time_corr_function, "Given an energy of energy difference fluctuations, calculates the time correlation function.");
+    m.def("calc_rdf", &calc_rdf, "Calculate the radial distribution function for a single time step.");
 }
