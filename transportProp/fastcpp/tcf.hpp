639e886d1d68b46ea3096c26773e2832058273eb
amritagos <amrita16thaug646@gmail.com>
2023-02-15 14:43:45 +0000

    fastcpp: Returns a row-ordered matrix now instead of vectors

--- transportProp/fastcpp/tcf.hpp
+++ transportProp/fastcpp/tcf.hpp
@@ -4,7 +4,9 @@
 #include <Eigen/Dense>
 #include <tuple>
 
-std::tuple<std::vector<double>, std::vector<double>, double>
+typedef Eigen::Matrix<double, Eigen::Dynamic, 2, Eigen::RowMajor> TCFMatrix;
+
+std::tuple<TCFMatrix, std::vector<double>, double>
 time_corr_function(Eigen::Ref<Eigen::RowVectorXd> energy_fluc, 
     Eigen::Ref<Eigen::RowVectorXd> time,
     int max_tau, int start_t0, int start_tau, int delta_tau);
