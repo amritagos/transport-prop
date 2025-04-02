88c85f79bf7b7840fdf8420c60a2adb39cdf4013
Amrita Goswami <amrita16thaug646@gmail.com>
2025-04-02 16:02:32 +0000

    build: Migrate to meson-python and meson
    Does not compile right now

--- transportProp/__init__.py
+++ transportProp/__init__.py
@@ -1,12 +1,2 @@
-"""python_project_template"""
-
-__version__ = "0.1.0"
-
-from .io import *
-from .msd import *
-from .misc import * 
-from .structures import * 
-from .cli import * 
-from .vacf import * 
-from .util import *
-from .fastcpp import *
+import transportProp.fastcpp
+from transportProp.fastcpp import *
